9175ed0336d03a7eadeda5494d9a537feace615b
Jacopo Nicolini <jacoponicolini7@gmail.com>
2025-04-10 16:18:10 +0200

    sync eagle

--- 8_mPNP_1D.py
+++ 8_mPNP_1D.py
@@ -66,7 +66,8 @@ def simulate_mPNP(Vapp, Vbulk, concentrations, plot_flag):
     Vapp_scaled = Vapp/phi_char
     V_bulk_scaled = V_bulk/phi_char
     L_scaled = L/x_char
-    # L_scaled = 1
+    #L_scaled = 35
+    print(x_char)
 
 
 
@@ -122,25 +123,48 @@ def simulate_mPNP(Vapp, Vbulk, concentrations, plot_flag):
     # u.sub(2).interpolate(V0_init)
 
     n = ufl.FacetNormal(domain)
+    def boundary_R(x):
+        return np.isclose(x[0], L_scaled)
 
+    def boundary_L(x):
+        return np.isclose(x[0], 0)
+
+
+    # Trova i facet a destra
+    facets_right = dolfinx.mesh.locate_entities_boundary(domain, domain.topology.dim - 1, boundary_R)
+    tags_right = np.full(len(facets_right), 1, dtype=np.int32)
+
+    # Trova i facet a sinistra
+    facets_left = dolfinx.mesh.locate_entities_boundary(domain, domain.topology.dim - 1, boundary_L)
+    tags_left = np.full(len(facets_left), 2, dtype=np.int32)
+
+    # Unisci i facet e i tag
+    all_facets = np.concatenate([facets_right, facets_left])
+    all_tags = np.concatenate([tags_right, tags_left])
+
+    # Crea un unico meshtag
+    facet_indices = np.argsort(all_facets)  # ordine crescente richiesto
+    marked_facets = dolfinx.mesh.meshtags(domain, domain.topology.dim - 1,
+                            all_facets[facet_indices],
+                            all_tags[facet_indices])
+
+    # Usa questo per creare un solo ds
+    ds = ufl.Measure("ds", domain=domain, subdomain_data=marked_facets)
     # MODIFIED PNP EQUATION SET
 
     # F1 = -ufl.inner(ufl.grad(c1)[0], v1) * ufl.dx - ufl.inner(c1 * ufl.grad(phi)[0], v1) * ufl.dx - ufl.inner((c1/(1-c1-c2))*(ufl.grad(c1)[0]+ ufl.grad(c2)[0]), v1)*ufl.dx 
     # F2 = -ufl.inner(ufl.grad(c2)[0], v2) * ufl.dx + ufl.inner(c2 * ufl.grad(phi)[0], v2) * ufl.dx - ufl.inner((c2/(1-c1-c2))*(ufl.grad(c1)[0]+ ufl.grad(c2)[0]), v2)*ufl.dx
 
     F7 = ufl.inner(ufl.grad(phi), ufl.grad(vphi)) * ufl.dx - ufl.inner((c1-c2), vphi) * ufl.dx
-    F8 = ufl.inner(-0.5*ufl.grad(c1) - c1 * ufl.grad(phi) - (c1/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2)), ufl.grad(v1)) * ufl.dx
-    F9 = ufl.inner(-0.5*ufl.grad(c2) + c2 * ufl.grad(phi) - (c2/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2)), ufl.grad(v2)) * ufl.dx
-    F8_SS =  ufl.inner(-0.5*ufl.grad(c1) - c1 * ufl.grad(phi) +c1*(c1-c2)*ufl.grad(phi), ufl.grad(v1)) * ufl.dx
-    F9_SS = ufl.inner(-0.5*ufl.grad(c2) +c2*ufl.grad(phi) +c2*(c1-c2)*ufl.grad(phi), ufl.grad(v2))*ufl.dx
-    F = F7 + F8 + F9
+    F8 = ufl.inner(-ufl.grad(c1) - c1 * ufl.grad(phi) - (c1/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2)), ufl.grad(v1)) * ufl.dx #- ufl.dot(ufl.grad(ufl.exp(phi)-ufl.exp(-phi)), n)*v1*ds(1) #- ufl.dot(ufl.grad(ufl.exp(phi)-ufl.exp(-phi)), n)*v1*ds(2)
+    F9 = ufl.inner(-ufl.grad(c2) + c2 * ufl.grad(phi) - (c2/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2)) , ufl.grad(v2)) * ufl.dx #- ufl.dot(ufl.grad(ufl.exp(phi)-ufl.exp(-phi)), n)*v2*ds(1) #- ufl.dot(ufl.grad(ufl.exp(phi)-ufl.exp(-phi)), n)*v2*ds(2)
+    F8_SS =  ufl.inner(-ufl.grad(c1) - c1 * ufl.grad(phi) +c1*(c1-c2)*ufl.grad(phi), ufl.grad(v1)) * ufl.dx
+    F9_SS = ufl.inner(-ufl.grad(c2) +c2*ufl.grad(phi) +c2*(c1-c2)*ufl.grad(phi), ufl.grad(v2))*ufl.dx
+    F_SS = F7 + F8_SS + F9_SS
+    F_NSS = F7 + F8 + F9 
 
     # SET BOUNDARY CONDITIONS 
-    def boundary_R(x):
-        return np.isclose(x[0], L_scaled)
 
-    def boundary_L(x):
-        return np.isclose(x[0], 0)
 
     V_split = V.sub(2)
     V_potential, _ = V_split.collapse()
@@ -210,15 +234,15 @@ def simulate_mPNP(Vapp, Vbulk, concentrations, plot_flag):
     # SET PROBLEM
 
     # Create nonlinear problem
-    problem = NonlinearPDE_SNESProblem(F, u, bcs)
+    problem_SS = NonlinearPDE_SNESProblem(F_SS, u, bcs)
 
     b = dolfinx.la.create_petsc_vector(V.dofmap.index_map, V.dofmap.index_map_bs)
-    J =fem.petsc.create_matrix(problem.a)
+    J =fem.petsc.create_matrix(problem_SS.a)
 
     # Create SNES solver and solve
     snes = PETSc.SNES().create()
-    snes.setFunction(problem.F, b)
-    snes.setJacobian(problem.J, J)
+    snes.setFunction(problem_SS.F, b)
+    snes.setJacobian(problem_SS.J, J)
 
     snes.setTolerances(rtol=1.0e-9, max_it=200)
     snes.getKSP().setType("gmres")
@@ -272,19 +296,19 @@ def simulate_mPNP(Vapp, Vbulk, concentrations, plot_flag):
 
     # print(np.shape(points_gathered))
     # print(np.shape(cells_gathered))
-    c1_local = u.sub(0).eval(points_gathered[0], cells_gathered[0])*c_char
-    c2_local = u.sub(1).eval(points_gathered[0], cells_gathered[0])*c_char
-    phi_local = u.sub(2).eval(points_gathered[0], cells_gathered[0])*phi_char
+    c1_local = u.sub(0).eval(points_gathered[0], cells_gathered[0])#*c_char
+    c2_local = u.sub(1).eval(points_gathered[0], cells_gathered[0])#*c_char
+    phi_local = u.sub(2).eval(points_gathered[0], cells_gathered[0])#*phi_char
 
-    J1 = -0.5*ufl.grad(c1) - c1 * ufl.grad(phi) - (c1/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
-    # J1 = -0.5*ufl.grad(c1) - c1 * ufl.grad(phi) +c1*(c1-c2)*ufl.grad(phi)
+    # J1 = -ufl.grad(c1) - c1 * ufl.grad(phi) - (c1/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
+    J1 = -ufl.grad(c1) - c1 * ufl.grad(phi) +c1*(c1-c2)*ufl.grad(phi)
     expr_J1 = fem.Expression(J1, points)
-    j1_local = expr_J1.eval(domain,cells_local)*J1_char*(x_char**2)
+    j1_local = expr_J1.eval(domain,cells_local)#*J1_char*(x_char**2)
 
-    J2 = -0.5*ufl.grad(c2) + c2 * ufl.grad(phi) - (c2/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
-    # J2 = -0.5*ufl.grad(c2) +c2*ufl.grad(phi) +c2*(c1-c2)*ufl.grad(phi)
+    # J2 = -ufl.grad(c2) + c2 * ufl.grad(phi) - (c2/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
+    J2 = -ufl.grad(c2) +c2*ufl.grad(phi) +c2*(c1-c2)*ufl.grad(phi)
     expr_J2 = fem.Expression(J2,points)
-    j2_local = expr_J2.eval(domain,cells_local)*J2_char*(x_char**2)
+    j2_local = expr_J2.eval(domain,cells_local)#*J2_char*(x_char**2)
 
 
 
@@ -315,91 +339,178 @@ def simulate_mPNP(Vapp, Vbulk, concentrations, plot_flag):
         c2_combined = c2_combined[sort_indices]
         phi_combined = phi_combined[sort_indices]
         
-        # print("Simulated concentration")
-        # print(c2_combined[0])
-        # print("Theoretical concentration")
-        # print(c_bulk*np.exp(phi_combined[0]/(k*T/q)) / (1- 2*NA*d**3*c_bulk + 2*NA*d**3*c_bulk*np.cosh(phi_combined[0]/(k*T/q))))
-        # print("PNP concentration")
-        # print(c_bulk*np.exp(phi_combined[0]/(k*T/q)))
+
 
         if plot_flag:
-            # fig = plt.figure()
-            # plt.plot(points_combined[:, 0], c1_combined, "k", linewidth=2, label="c1")
-            # plt.plot(points_combined[:, 0], c2_combined, "b", linewidth=2, label="c2")
-            # #plt.plot(points_combined[:,0]*x_char, c_bulk*np.exp(phi_combined/(k*T/q)) / (1- 2*NA*d**3*c_bulk + 2*NA*d**3*c_bulk*np.cosh(phi_combined/(k*T/q))), "green", label="c2 theoretical")
-            # plt.xscale("linear")
 
-            # plt.grid(True)
-            # plt.xlabel("x")
-            # plt.legend()
+            fig, ((ax1, ax2), (ax3, ax4)) =plt.subplots(2,2, figsize=(10,10))
+            ax3.plot(points_combined[:, 0], phi_combined, color="orange", linewidth=2, label="H full")
+            ax3.grid()
+            ax3.legend()
+            ax4.plot(coords, j1_combined+j2_combined, color="green", linestyle="dashed", linewidth=2, label="jtot full")
+            ax4.plot(coords, c1_combined+c2_combined, color="green", linestyle="solid", linewidth=2, label="ctot full")
+            ax4.plot(coords, c1_combined-c2_combined, color="purple", linestyle="solid", linewidth=2, label="charge full")
+
+            ax4.grid()
+            ax4.legend()
+            ax1.plot(points_combined[:, 0], j1_combined, "r", linestyle="dashed", linewidth=2, label="j1 full")
+            ax1.plot(points_combined[:,0], c1_combined, "r", linestyle="solid", linewidth=2, label="c1 full")
+            ax1.grid()
+            ax1.legend()
+            ax2.plot(points_combined[:, 0], j2_combined, "b", linestyle="dashed", linewidth=2, label="j2 full")
+            ax2.plot(points_combined[:,0], c2_combined, "b", linestyle="solid", linewidth=2, label="c2 full")
+            ax2.grid()
+            ax2.legend()
+            # fig.savefig("esempio_Fenics.png", dpi=300, bbox_inches="tight")
+
+
+
+            print("Done pt 1")
+
+        # column_names = ["x", "Potential", "J1", "J2", "C1", "C2"]
+        # data = np.hstack((coords, phi_combined, j1_combined, j2_combined, c1_combined, c2_combined))
+
+        # df = pd.DataFrame(data=data,columns=column_names)
+
+
+
+        # Create nonlinear problem
+    
+    
+    
+    problem_NSS = NonlinearPDE_SNESProblem(F_NSS, u, bcs)
+
+    b = dolfinx.la.create_petsc_vector(V.dofmap.index_map, V.dofmap.index_map_bs)
+    J =fem.petsc.create_matrix(problem_NSS.a)
+
+    # Create SNES solver and solve
+    snes = PETSc.SNES().create()
+    snes.setFunction(problem_NSS.F, b)
+    snes.setJacobian(problem_NSS.J, J)
+
+    snes.setTolerances(rtol=1.0e-9, max_it=200)
+    snes.getKSP().setType("gmres")
+    snes.getKSP().setTolerances(rtol=1.0e-9)
+    snes.getKSP().getPC().setType("lu")
+
+    # For SNES line search to function correctly it is necessary that the
+    # u.x.petsc_vec in the Jacobian and residual is *not* passed to
+    # snes.solve.
+    x = u.x.petsc_vec.copy()
+    x.ghostUpdate(addv=PETSc.InsertMode.INSERT, mode=PETSc.ScatterMode.FORWARD)
+    dolfinx.log.set_log_level(dolfinx.cpp.log.LogLevel.INFO)
+
+    snes.solve(None, x)
+    assert snes.getConvergedReason() > 0
+
+    u.x.scatter_forward()
+    dolfinx.log.set_log_level(dolfinx.cpp.log.LogLevel.ERROR)
+
+    # POSTPROCESS
+
+    #points_on_proc, cells = extract_central_cutline_1D(domain, L_scaled)
+
+    points = np.array([[0.5]], dtype=np.float64)
+    num_cells_local = domain.topology.index_map(domain.topology.dim).size_local
+    cells_local = range(0,num_cells_local-1)
+
+    x_expr = dolfinx.fem.Expression(ufl.SpatialCoordinate(domain), points)
+    coords = x_expr.eval(domain, cells_local)
+    # coords = coords[0:len(coords)-1]
+
+    points_3D = np.zeros((3,len(coords)))  
+    points_3D[0] = coords[:,0]
+
+    bb_tree = dolfinx.geometry.bb_tree(domain, domain.topology.dim)
+    cells = []
+    points_on_proc = []
+    # Find cells whose bounding-box collide with the the points
+    cell_candidates = dolfinx.geometry.compute_collisions_points(bb_tree, points_3D.T)
+    # Choose one of the cells that contains the point
+    colliding_cells = dolfinx.geometry.compute_colliding_cells(domain, cell_candidates, points_3D.T)
+    for i, point in enumerate(points_3D.T):
+        if len(colliding_cells.links(i)) > 0:
+            points_on_proc.append(point)
+            cells.append(colliding_cells.links(i)[0])
+
+    points_on_proc = np.array(points_on_proc, dtype=np.float64)
+
+    points_gathered = MPI.COMM_WORLD.gather(points_on_proc, root=0)
+    cells_gathered = MPI.COMM_WORLD.gather(cells, root=0)
+
+    # print(np.shape(points_gathered))
+    # print(np.shape(cells_gathered))
+    c1_local = u.sub(0).eval(points_gathered[0], cells_gathered[0])#*c_char
+    c2_local = u.sub(1).eval(points_gathered[0], cells_gathered[0])#*c_char
+    phi_local = u.sub(2).eval(points_gathered[0], cells_gathered[0])#*phi_char
+
+    J1 = -ufl.grad(c1) - c1 * ufl.grad(phi) - (c1/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
+    # J1 = -ufl.grad(c1) - c1 * ufl.grad(phi) +c1*(c1-c2)*ufl.grad(phi)
+    expr_J1 = fem.Expression(J1, points)
+    j1_local = expr_J1.eval(domain,cells_local)#*J1_char*(x_char**2)
+
+    J2 = -ufl.grad(c2) + c2 * ufl.grad(phi) - (c2/(1-c1-c2))*(ufl.grad(c1)+ ufl.grad(c2))
+    # J2 = -ufl.grad(c2) +c2*ufl.grad(phi) +c2*(c1-c2)*ufl.grad(phi)
+    expr_J2 = fem.Expression(J2,points)
+    j2_local = expr_J2.eval(domain,cells_local)#*J2_char*(x_char**2)
+
+
+
+
+    c1_gathered = comm.gather(c1_local,root=0)
+    c2_gathered = comm.gather(c2_local,root=0)
+    phi_gathered = comm.gather(phi_local,root=0)
+    j1_gathered = comm.gather(j1_local, root=0)
+    j2_gathered = comm.gather(j2_local, root=0)
+
+
+    if MPI.COMM_WORLD.rank == 0:  
+
+        points_gathered_filtered = [p for p in points_gathered if p.size > 0]
+        if len(points_gathered_filtered) > 0:
+            points_combined = np.vstack(points_gathered_filtered)  
+        else:
+            points_combined = points_gathered_filtered
+        c1_combined = np.vstack(c1_gathered)
+        c2_combined = np.vstack(c2_gathered)
+        phi_combined = np.vstack(phi_gathered)
+        j1_combined = np.vstack(j1_gathered)
+        j2_combined = np.vstack(j2_gathered)
+
+        sort_indices = np.argsort(points_combined[:, 0])
+        points_combined = points_combined[sort_indices]
+        c1_combined = c1_combined[sort_indices]
+        c2_combined = c2_combined[sort_indices]
+        phi_combined = phi_combined[sort_indices]
+        
+
+        if plot_flag:
+
             
             c1_theoretical = c_bulk_scaled*np.exp(-phi_combined) / (1- 2*c_bulk_scaled + 2*c_bulk_scaled*np.cosh(phi_combined))
             c2_theoretical = c_bulk_scaled*np.exp(phi_combined) / (1- 2*c_bulk_scaled + 2*c_bulk_scaled*np.cosh(phi_combined))
 
-            fig, ((ax1, ax2), (ax3, ax4)) =plt.subplots(2,2)
-            ax3.plot(points_combined[:, 0]*x_char, phi_combined, color="orange", linewidth=2, label="H")
-            ax3.grid()
+            # fig, ((ax1, ax2), (ax3, ax4)) =plt.subplots(2,2)
+            ax3.plot(points_combined[:, 0], phi_combined, color="orange", linestyle = "None", marker="D", markevery=5000, label="H PB")
             ax3.legend()
-            ax4.plot(coords*x_char, j1_combined+j2_combined, color="green", linestyle="dashed", linewidth=2, label="jtot")
-            ax4.plot(coords*x_char, c1_combined+c2_combined, color="green", linestyle="solid", linewidth=2, label="rhotot")
-            ax4.grid()
+            ax4.plot(coords, j1_combined+j2_combined, color="green", linestyle="None", marker="v", markevery=5000, label="jtot PB")
+            ax4.plot(coords, c1_combined+c2_combined, color="green", linestyle="None", marker="o", markevery=5000, label="ctot PB")
+            ax4.plot(coords, c1_combined-c2_combined, color="purple", linestyle="None", marker="o", markevery=5000, label="charge PB")
+
             ax4.legend()
-            ax1.plot(points_combined[:, 0]*x_char, j1_combined, "r", linestyle="dashed", linewidth=2, label="j1")
-            ax1.plot(points_combined[:,0]*x_char, c1_combined, "r", linestyle="solid", linewidth=2, label="rho1")
-            # ax1.plot(points_combined[:,0], c1_theoretical, "black", label="rho1 theoretical")
-            ax1.grid()
+            ax1.plot(points_combined[:, 0], j1_combined, color="red", linestyle="None", marker="v", markevery=5000, label="j1 PB")
+            ax1.plot(points_combined[:,0], c1_combined, color="red", linestyle="None", marker="o", markevery=5000, label="c1 PB")
             ax1.legend()
-            ax2.plot(points_combined[:, 0]*x_char, j2_combined, "b", linestyle="dashed", linewidth=2, label="j2")
-            ax2.plot(points_combined[:,0]*x_char, c2_combined, "b", linestyle="solid", linewidth=2, label="rho2")
-            # ax2.plot(points_combined[:,0], c2_theoretical, "black", label="rho2 theoretical")
-            ax2.grid()
+            ax2.plot(points_combined[:, 0], j2_combined, color="blue", linestyle="None", marker="v", markevery=5000, label="j2 PB")
+            ax2.plot(points_combined[:,0], c2_combined, color="blue", linestyle="None", marker="o", markevery=5000, label="c2 PB")
             ax2.legend()
             fig.savefig("esempio_Fenics.png", dpi=300, bbox_inches="tight")
-
-            # fig2 = plt.figure()
-            # plt.plot(points_combined[:, 0], phi_combined, color="orange", linewidth=2, label="H")
-            # plt.grid(True)
-            # plt.xlabel("x")
-            # plt.xscale("linear")
-            # plt.legend()
-
-
-            # fig3 = plt.figure()
-            # plt.plot(coords, j1_combined+j2_combined, color="green", linestyle="dashed", linewidth=2, label="jtot")
-            # plt.plot(coords, c1_combined+c2_combined, color="green", linestyle="solid", linewidth=2, label="rhotot")
-            # plt.xlabel("x")
-            # plt.grid(True)
-            # plt.xscale("linear")
-            # plt.yscale("linear")
-            # plt.legend()
-
-            # fig4 = plt.figure()
-            # plt.plot(points_combined[:, 0], j1_combined, "r", linestyle="dashed", linewidth=2, label="j1")
-            # plt.plot(points_combined[:,0], c1_combined, "r", linestyle="solid", linewidth=2, label="rho1")
-            # plt.plot(points_combined[:,0], c1_theoretical, "black", label="rho1 theoretical")
-            # plt.grid(True)
-            # plt.xlabel("x")
-            # plt.xscale("linear")
-            # plt.legend()
-
-            # fig5 = plt.figure()
-            # plt.plot(points_combined[:, 0], j2_combined, "b", linestyle="dashed", linewidth=2, label="j2")
-            # plt.plot(points_combined[:,0], c2_combined, "b", linestyle="solid", linewidth=2, label="rho2")
-            # plt.plot(points_combined[:,0], c2_theoretical, "black", label="rho2 theoretical")
-            # plt.grid(True)
-            # plt.xlabel("x")
-            # plt.xscale("linear")
-            # plt.legend()
-
-            print("Done")
+            print("Done pt 2")  
             plt.show()
 
-        column_names = ["x", "Potential", "J1", "J2", "C1", "C2"]
-        data = np.hstack((coords, phi_combined, j1_combined, j2_combined, c1_combined, c2_combined))
 
-        df = pd.DataFrame(data=data,columns=column_names)
-
-        return df
+            
+    # return df
 
 
 
@@ -418,6 +529,6 @@ if __name__ == "__main__":
             self.use_surf_bc = use_surf_bc
 
     # Simulation conditions
-    Vapp = k*T/q
-    concentrations = Concentration_BC(170, c_bulk1=4e-4, c_bulk2=4e-4, c_surf1=8e-4, c_surf2=1e-4, use_surf_bc=False)
+    Vapp = -2*k*T/q
+    concentrations = Concentration_BC(170, c_bulk1=0.4, c_bulk2=0.4, c_surf1=0.8, c_surf2=0.1, use_surf_bc=True)
     simulate_mPNP(Vapp, 0,concentrations, plot_flag=True)
